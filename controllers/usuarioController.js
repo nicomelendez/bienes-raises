b29955332de241146315ea90f42ee08f70bc79e0
Nicolás Meléndez <nicomelendez10@gmail.com>
2022-09-05 21:04:19 -0300

    validaciones

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import {check, validationResult} from 'express-validator'
 import Usuario from "../models/Usuario.js"
 
 const formularioLogin =  (req,res) => {
@@ -14,6 +15,26 @@ const formularioRegistro =  (req,res) => {
 
 const registrar = async (req, res)=>{
 
+    //Validacion
+    await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req)
+    await check('email').isEmail().withMessage('El email no es valido').run(req)
+    await check('password').isLength({min:6}).withMessage('La contraseña debe ser de al menos 6 caracteres').run(req)
+    await check('repetir_password').equals('password').withMessage('Las contraseña no son iguales').run(req)
+
+    let resultado = validationResult(req)
+    //verificar si el resulta esta vacio
+
+    if(!resultado.isEmpty()){
+        //Errores
+        return res.render('auth/registro',{
+            pagina: 'Error',
+            errores: resultado.array(),
+            usuario: {
+                nombre:req.body.nombre,
+                email:req.body.email
+            }
+        })
+    }
     const usuario = await Usuario.create(req.body);
     res.json(usuario);
 }
