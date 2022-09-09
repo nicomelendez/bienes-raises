861c5c827b5a0ad6f1524f6db955423144408f24
Nicolás Meléndez <nicomelendez10@gmail.com>
2022-09-09 20:38:40 -0300

    apartado mis propiedades

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import csrf from 'csurf';
 import cookieParser from 'cookie-parser';
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import propiedadesRoutes from './routes/propiedadesRoutes.js'
 import db from './config/db.js'
 
 //Crear la app
@@ -35,6 +36,8 @@ app.use( express.static('public') )
 
 // Routing
 app.use('/auth', usuarioRoutes);
+
+app.use('/', propiedadesRoutes);
 
 
 
