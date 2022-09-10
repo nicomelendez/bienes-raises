768e9d3f41b423f2778d98afb63e5561dd711135
Nicolás Meléndez <nicomelendez10@gmail.com>
2022-09-10 20:57:18 -0300

    Entidad propiedad

--- models/Propiedad.js
+++ models/Propiedad.js
@@ -16,4 +16,38 @@ const Propiedad = db.define('propiedades', {
         type: DataTypes.TEXT,
         allowNull: false
     },
+    habitaciones: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    estacionamiento: {
+        type:DataTypes.INTEGER,
+        allowNull: false
+    },
+    banios: {
+        type:DataTypes.INTEGER,
+        allowNull:false
+    },
+    calle:{
+        taype:DataTypes.STRING(80),
+        allowNull:false
+    },
+    lat:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    lng:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    imagen:{
+        type:DataTypes.STRING,
+        allowNull: false
+    },
+    publicado:{
+        type:DataTypes.BOOLEAN,
+        allowNull:false,
+        defaultValue:false
+    }
+
 });
