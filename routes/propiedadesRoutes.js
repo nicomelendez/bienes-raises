1c16fa6da2b87acb61c3338c723a0da0788a6383
Nicolás Meléndez <nicomelendez10@gmail.com>
2022-09-19 19:21:53 -0300

    validaciones de propiedad

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -9,6 +9,15 @@ router.get('/mis-propiedades', admin)
 router.get('/propiedades/crear', crear)
 router.post('/propiedades/crear',  
     body('titulo').notEmpty().withMessage('El titulo del anuncio es obligatorio'),
+    body('descripcion')
+        .notEmpty().withMessage('La descripción no puede ir vacia')
+        .isLength({max:200}).withMessage('La descripción es muy larga'),
+    body('categoria').isNumeric().withMessage('Seleccione una categoría'),
+    body('precio').isNumeric().withMessage('Seleccione un rango de precio'),
+    body('habitaciones').isNumeric().withMessage('Seleccione la cantidad de habitaciones'),
+    body('estacionamiento').isNumeric().withMessage('Seleccione la cantidad de estacionamientos'),
+    body('banios').isNumeric().withMessage('Seleccione la cantidad de baños'),
+    body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa'),
     guardar
 )
 export default router
